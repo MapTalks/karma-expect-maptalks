8742a45513de29847b94749ba72f29c9a76c9239
FuZhenn <fuzhen@maptalks.org>
2016-06-14 16:54:22 +0800

    fixed karma loading and bump to 0.1.2

--- index.js
+++ index.js
@@ -6,7 +6,24 @@ var pattern = function (file) {
 };
 
 var framework = function (files) {
-  files.unshift(pattern(path.resolve(require.resolve('expect-maptalks'))));
+    var index = -1,
+        postfix = path.join('expect.js' , 'index.js'),
+        pa;
+    for (var i = 0; i < files.length; i++) {
+        pa = files[i].pattern;
+        if (pa.length > postfix.length && pa.substring(pa.length - postfix.length) === postfix) {
+            index = i;
+            break;
+        }
+    }
+    if (index === -1) {
+        throw new Error('expect-maptalks can not find expect.js.');
+    }
+    if (index === files.length - 1) {
+        files.push(pattern(path.resolve(require.resolve('expect-maptalks'))));
+    } else {
+        files.splice(index + 1, 0, pattern(path.resolve(require.resolve('expect-maptalks'))));
+    }
 };
 
 framework.$inject = ['config.files'];
